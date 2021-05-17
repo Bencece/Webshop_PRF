7d0e7caf1a7789cb2d75886517baaf59bd015a91
Bencece <zahoranbence@hotmail.com>
2021-05-17 16:42:53 +0200

    Added the spring server

---
+++ spring-server/src/test/java/com/webshopprf2021/springserver/SpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webshopprf2021.springserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
